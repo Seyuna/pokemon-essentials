d9696deec1457b302a439f677c48e1627f34737f
Golisopod-User <>
2021-01-31 14:28:33 +0530

    Add increased Size

--- Data/Scripts/023_PluginScripts/026_EBS Vs Animations.rb
+++ Data/Scripts/023_PluginScripts/026_EBS Vs Animations.rb
@@ -461,6 +461,7 @@ def vsSequenceSpecial(viewport,trainername,trainerid,tbargraphic,tgraphic)
   name.bitmap.font.size = 48
   name.visible = false
   pbSetSystemFont(name.bitmap)
+  name.bitmap.font.size = 69
   pbDrawOutlineText(name.bitmap,64,viewport.rect.height-160,-1,-1,"#{trainername}",Color.new(255,255,255),Color.new(0,0,0),2)
   names.bitmap = name.bitmap.clone
 
