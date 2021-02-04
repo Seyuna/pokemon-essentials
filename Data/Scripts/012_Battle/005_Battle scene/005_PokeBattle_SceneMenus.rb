2a4e9d2d46673917b9048900680ee099fa6e27e7
CamThundaga <72353508+CamThundaga@users.noreply.github.com>
2021-02-03 20:43:13 -0800

    bugfix for last push real quick

--- Data/Scripts/012_Battle/005_Battle scene/005_PokeBattle_SceneMenus.rb
+++ Data/Scripts/012_Battle/005_Battle scene/005_PokeBattle_SceneMenus.rb
@@ -408,9 +408,9 @@ class FightMenuDisplay < BattleMenuBase
     @visibility["typeIcon"] = true
     # Type icon
     #thundaga move type, improved by GolisopodUser
-    #mType = (pbGetMoveData(move.id,MOVE_FLAGS).include?("z"))? @battler.type1 : move.type
+    #mType = (pbGetMoveData(move.id,MOVE_FLAGS).include?("z"))? @pokemon.type1 : move.type
+    #@typeIcon.src_rect.y = move.pbCalcType(@battler)*TYPE_ICON_HEIGHT
     @typeIcon.src_rect.y = move.pbCalcType(@battler)*TYPE_ICON_HEIGHT
-    @typeIcon.src_rect.y = mType*TYPE_ICON_HEIGHT
     # PP text
     if move.totalpp>0
       ppFraction = [(4.0*move.pp/move.totalpp).ceil,3].min
