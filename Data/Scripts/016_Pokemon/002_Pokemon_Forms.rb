718844fc6e48812d11543bd6d1f44fb76baf02c5
CamThundaga <72353508+CamThundaga@users.noreply.github.com>
2021-02-08 15:20:38 -0800

    Staconan Durant Added

--- Data/Scripts/016_Pokemon/002_Pokemon_Forms.rb
+++ Data/Scripts/016_Pokemon/002_Pokemon_Forms.rb
@@ -1672,3 +1672,14 @@ MultipleForms.register(:LEDYBA,{
 }
 })
 MultipleForms.copy(:LEDYBA,:LEDIAN)
+
+MultipleForms.register(:DURANT,{
+"getFormOnCreation"=>proc{|pokemon|
+   maps=[1]
+   if $game_map && maps.include?($game_map.map_id)
+     next 0
+   else
+     next 1
+   end
+}
+})
